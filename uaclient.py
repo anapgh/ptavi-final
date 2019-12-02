664ee06aa3852b52e0bcaa0ff03c3ec3ce2e5b60
anapgh <ap.garcia.2016@alumnos.urjc.es>
2019-12-02 15:24:51 +0100

    hecho la lectura del xml, y ademas elaborado un poco el mensaje del REGISTER

---
+++ uaclient.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""
+Programa cliente UDP que abre un socket a un servidor
+"""
+import sys
+import socket
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+class SmallXMLHandler(ContentHandler):
+
+    def __init__(self):
+        self.dicc = {}
+        self.elemDict = {
+                        "account": ["username", "pass"],
+                        "uaserver": ["ip", "puerto"],
+                        "rtpaudio": ["puerto"],
+                        "regproxy": ["ip","puerto"],
+                        "log": ["path"],
+                        "audio": ["path"]
+                        }
+
+    def startElement(self, name, attrs):
+        """
+        Método que se llama cuando se abre una etiqueta
+        """
+        if name in self.elemDict:
+            for atributo in self.elemDict[name]:
+                self.dicc[name +'_'+ atributo] = attrs.get(atributo, "")
+
+    def get_tags(self):
+        return(self.dicc)
+
+
+if __name__ == "__main__":
+# Constantes. Dirección IP del servidor y contenido a enviar
+    try:
+        CONFIG = sys.argv[1]
+        METHOD  = sys.argv[2]
+        OPCION = sys.argv[3]
+    except (IndexError, ValueError):
+        sys.exit('Usage: client.py config method opcion ')
+
+# Con el manejador, creo un diccionario 'config' con el fichero xml
+    parser = make_parser()
+    cHandler = SmallXMLHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open(CONFIG))
+    config = cHandler.get_tags()
+
+# Compruebo que metodo me ha Introducido el cliente por la shell y elaboro el mensaje
+    if METHOD == 'REGISTER':
+        account_username = config['account_username']
+        uaserver_puerto = config['uaserver_puerto']
+        LINES = (METHOD + ' sip:'+ account_username + ':'+ uaserver_puerto + ' SIP/2.0 ' + 'Expires: '+ OPCION)
+    elif METHOD == 'INVITE':
+        print('invite')
+        LINES = (METHOD + ' sip:'+ account_username + ':'+ uaserver_puerto + ' SIP/2.0 ' + 'Expires: '+ OPCION)
+    elif METHOD == 'BYE':
+        print("bye")
+    else:
+        print("no vale")
+
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        regproxy_ip = config['regproxy_ip']
+        regproxy_puerto = int(config['regproxy_puerto'])
+        my_socket.connect((regproxy_ip, regproxy_puerto))
+        print("Enviando:", LINES)
+        my_socket.send(bytes(LINES, 'utf-8') + b'\r\n\r\n')
+        data = my_socket.recv(1024)
+        print('Recibido -- ', data.decode('utf-8'))
+
+    print("Socket terminado.")
