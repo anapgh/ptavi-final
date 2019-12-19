f10a29b9b6b80b65b9bf3cf0adb69a4be8664b5f
anapgh <ap.garcia.2016@alumnos.urjc.es>
2019-12-19 23:40:15 +0100

    pasado pep8

--- uaclient.py
+++ uaclient.py
@@ -25,7 +25,7 @@ class log_file():
     def log_start_finish(self, estado):
         """Write in log file 'Starting' or 'Finishing' """
         real_time = time.strftime('%Y-%m-%d %H:%M:%S',
-                                     time.gmtime(time.time()))
+                                  time.gmtime(time.time()))
         if estado == 'start':
             mess = (real_time + ' Starting...\r\n')
             self.write_log(mess)
@@ -36,35 +36,35 @@ class log_file():
     def log_sent(self, ip, port, message):
         """Write in log file for send messages."""
         real_time = time.strftime('%Y-%m-%d %H:%M:%S',
-                                     time.gmtime(time.time()))
+                                  time.gmtime(time.time()))
         message = message.replace('\r\n', ' ')
         mess = (real_time + ' Sent to ' + ip + ':' + str(port) +
-                  ': ' + message + '\r\n')
+                ': ' + message + '\r\n')
         self.write_log(mess)
 
     def log_received(self, ip, port, message):
         """Write in log file for received messages."""
         real_time = time.strftime('%Y-%m-%d %H:%M:%S',
-                                     time.gmtime(time.time()))
+                                  time.gmtime(time.time()))
         message = message.replace('\r\n', ' ')
         mess = (real_time + ' Received from ' + ip + ':' + str(port) +
-                  ': ' + message + '\r\n')
+                ': ' + message + '\r\n')
         self.write_log(mess)
 
     def log_rtp(self, ip, port, audio):
         """Write in log file for send rtp audio"""
         real_time = time.strftime('%Y-%m-%d %H:%M:%S',
-                                     time.gmtime(time.time()))
+                                  time.gmtime(time.time()))
         mess = (real_time + ' Senting to ' + ip + ':' + str(port) +
-                  ' file: ' + audio + ' by RTP\r\n')
+                ' file: ' + audio + ' by RTP\r\n')
         self.write_log(mess)
 
     def conexion_refused_error(self, ip, port):
         """For ConnectionRefusedError."""
         real_time = time.strftime('%Y-%m-%d %H:%M:%S',
-                                     time.gmtime(time.time()))
+                                  time.gmtime(time.time()))
         mess = (real_time + ' Error: No server listening at ' + ip +
-                  ' port ' + str(port) + '\r\n')
+                ' port ' + str(port) + '\r\n')
         self.write_log(mess)
 
 
@@ -78,7 +78,7 @@ class SmallXMLHandler(ContentHandler):
                         "account": ["username", "passwd"],
                         "uaserver": ["ip", "puerto"],
                         "rtpaudio": ["puerto"],
-                        "regproxy": ["ip","puerto"],
+                        "regproxy": ["ip", "puerto"],
                         "log": ["path"],
                         "audio": ["path"]
                         }
@@ -87,18 +87,18 @@ class SmallXMLHandler(ContentHandler):
         """ Method to open tag."""
         if name in self.elemDict:
             for atributo in self.elemDict[name]:
-                self.dicc[name +'_'+ atributo] = attrs.get(atributo, "")
+                self.dicc[name + '_' + atributo] = attrs.get(atributo, "")
 
     def get_tags(self):
         """ Method to return tag value. """
         return(self.dicc)
+
 
 def send_message(reply):
     """Send messages."""
     print("Enviando:", reply)
     my_socket.send(bytes(reply, 'utf-8') + b'\r\n\r\n')
     log.log_sent(REGPROXY_IP, REGPROXY_PUERTO, reply)
-
 
 
 def send_rtp(origen_ip, origen_puertortp):
@@ -112,22 +112,22 @@ def send_rtp(origen_ip, origen_puertortp):
 
 
 if __name__ == "__main__":
-    #Constantes. Fichero xml, metodo a usar y opcion de cada metodo
+    # Constantes. Fichero xml, metodo a usar y opcion de cada metodo
     try:
         CONFIG = sys.argv[1]
-        METHOD  = sys.argv[2]
+        METHOD = sys.argv[2]
         OPCION = sys.argv[3]
     except (IndexError, ValueError):
         sys.exit('Usage: client.py config method opcion ')
 
-    #Con el manejador, creo un diccionario 'config' con el fichero xml
+    # Con el manejador, creo un diccionario 'config' con el fichero xml
     parser = make_parser()
     cHandler = SmallXMLHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open(CONFIG))
     config = cHandler.get_tags()
 
-    #Compruebo que metodo me ha Introducido el cliente por la shell y elaboro el mensaje
+    # Compruebo que metodo se ha introducido por la shell y elaboro el mensaje
     ACCOUNT_USERNAME = config['account_username']
     ACCOUNT_PASSWD = config['account_passwd']
     UASERVER_IP = config['uaserver_ip']
@@ -138,34 +138,35 @@ if __name__ == "__main__":
     LOG_PATH = config['log_path']
     AUDIO_PATH = config['audio_path']
 
-    #Creo el log
+    # Creo el log
     log = log_file()
     estado = 'start'
     log.log_start_finish(estado)
 
-    #Envio los mensajes segun el metodo
+    # Envio los mensajes segun el metodo
     if METHOD == 'REGISTER':
-        LINES = (METHOD + ' sip:'+ ACCOUNT_USERNAME + ':'+ UASERVER_PUERTO + ' SIP/2.0 ' + 'Expires: '+ OPCION)
+        LINES = (METHOD + ' sip:' + ACCOUNT_USERNAME + ':' + UASERVER_PUERTO +
+                 ' SIP/2.0 ' + 'Expires: ' + OPCION)
     elif METHOD == 'INVITE':
-        LINES = (METHOD + ' sip:'+ OPCION + ' SIP/2.0\r\n\r\n')
+        LINES = (METHOD + ' sip:' + OPCION + ' SIP/2.0\r\n\r\n')
         LINES = LINES + ('Content-Type: application/sdp\r\n\r\n')
         LINES = LINES + ('v=0\r\n')
         LINES = LINES + ('o=' + ACCOUNT_USERNAME + ' ' + UASERVER_IP + '\r\n')
         LINES = LINES + ('s=misesion\r\n')
         LINES = LINES + ('t=0\r\n')
-        LINES = LINES + ('m=audio ' + RTPAUDIO_PUERTO  + ' RTP')
+        LINES = LINES + ('m=audio ' + RTPAUDIO_PUERTO + ' RTP')
     elif METHOD == 'BYE':
         LINES = (METHOD + ' sip:' + OPCION + ' SIP/2.0')
     else:
         LINES = (METHOD + ' sip:' + OPCION + ' SIP/2.0')
 
-    #Conecto con el servidor
+    # Conecto con el servidor
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         try:
             my_socket.connect((REGPROXY_IP, REGPROXY_PUERTO))
-            send_message(LINES) # Envio el mensaje
+            send_message(LINES)  # Envio el mensaje
             data = my_socket.recv(1024)
-            reply  = data.decode('utf-8')
+            reply = data.decode('utf-8')
             print('Recibido -- ', reply)
         except ConnectionRefusedError:
             log.conexion_refused_error(REGPROXY_IP, REGPROXY_PUERTO)
@@ -174,33 +175,33 @@ if __name__ == "__main__":
             sys.exit('Error: No server listening at ' + REGPROXY_IP +
                      ' port ' + str(REGPROXY_PUERTO))
 
-        #Enviamos la autorización al proxy registrar.
+        # Enviamos la autorización al proxy registrar.
         if reply.split('\r\n\r\n')[0] == 'SIP/2.0 401 Unauthorized':
             authenticate = reply.split('\r\n\r\n')[1]
             nonce = authenticate.split('"')[1]
-            h = hashlib.md5(bytes(ACCOUNT_PASSWD  + '\r\n', 'utf-8'))
+            h = hashlib.md5(bytes(ACCOUNT_PASSWD + '\r\n', 'utf-8'))
             h.update(bytes(nonce, 'utf-8'))
             digest = h.hexdigest()
-            LINE = (METHOD + ' sip:'+ ACCOUNT_USERNAME + ':'+
-                UASERVER_PUERTO + ' SIP/2.0 ' + 'Expires: '+ OPCION + '\r\n\r\n')
+            LINE = (METHOD + ' sip:' + ACCOUNT_USERNAME + ':' +
+                    UASERVER_PUERTO + ' SIP/2.0 ' + 'Expires: ' + OPCION +
+                    '\r\n\r\n')
             LINE += ('Authorization: Digest response="' + digest + '"')
             send_message(LINE)
             data = my_socket.recv(1024)
             reply = data.decode('utf-8')
             log.log_received(REGPROXY_IP, REGPROXY_PUERTO, reply)
-        #Enviamos el mensaje ACK.
+        # Enviamos el mensaje ACK.
         elif METHOD == 'INVITE':
             if reply.split('\r\n\r\n')[0] == 'SIP/2.0 100 Trying':
                 if reply.split('\r\n\r\n')[1] == 'SIP/2.0 180 Ringing':
                     if reply.split('\r\n\r\n')[2] == 'SIP/2.0 200 OK':
                         LINE = ('ACK' + ' sip:' + OPCION + ' SIP/2.0')
                         send_message(LINE)
-                        #Del sdp de la otra maquina sacamos su ip y su puerto
+                        # Del sdp de la otra maquina sacamos su ip y su puerto
                         sdp = reply.split('\r\n\r\n')[4].split('\r\n')
                         origen_ip = sdp[1].split(' ')[1]
                         origen_puertortp = sdp[4].split(' ')[1]
-                        #Hago el envio de multimedia por RTP
+                        # Hago el envio de multimedia por RTP
                         send_rtp(origen_ip, origen_puertortp)
-
 
     print("Socket terminado.")
